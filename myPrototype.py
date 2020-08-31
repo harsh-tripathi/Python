f067afc47e1b7d3910d0d0e210c5c01c84c22fa3
Harsh Tripathi <lko.atripathi@gmail.com>
2020-08-31 14:38:34 +0530

    Comments, Escape Sequences

---
+++ myPrototype.py
@@ -0,0 +1,20 @@
+#This is a comment
+'''This is a comment
+Author: Thomas
+Date: 27 November 2020
+Multi-line comment ends here
+'''
+print("Main code started")
+
+# print statement for printing strings
+print("Thomas is a programmer", end="**")
+
+# Print statement with a literal
+print(1+87)
+
+#This will print "Thomas is a programmer**88" on the screen 
+
+# print("If Nothing Changes, Nothing Changes","Nothing Changes")
+# print("next line")
+# print("C:\'narry")
+print("Thomas is \n good boy \t1") #comment after statement
