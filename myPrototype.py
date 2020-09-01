ac806881e1ca4ed553474f9d95539dbdb338b0e8
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-01 15:04:32 +0530

    String Slicing And Other Functions

--- myPrototype.py
+++ myPrototype.py
@@ -1,23 +1,8 @@
-var1 = "54"
-var4 = "32"
-var2 = 4        #it is a int variable
-var3 = 36.7     #it is a float variable
-# print(100 * str(int(var1) + int(var4)) )
-# print(100 * "Hello world\n")
-# print("Enter your number")
-# inpnum = input()
-#
-# print("You entered", int(inpnum)+10)
-"""
-str()
-int()
-float()
-"""
+mystr = "Harry is a good boy"
+# print(len(mystr))
+# print(mystr[::-2])
 
-# print(type(var1))
-
-print("Enter first number")
-n1 = input()
-print("Enter second number")
-n2 = input()
-print("Sum of these two numbers is", int(n1) + int(n2))
+print(mystr.endswith("bdoy"))
+print(mystr.count("o"))
+print(mystr.capitalize())
+print(mystr.replace("is", "are"))
