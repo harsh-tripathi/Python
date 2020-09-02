03633dd1845b518a743d0ebfd980b9edabf9ca39
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-02 14:55:38 +0530

    Sets In Python

--- myPrototype.py
+++ myPrototype.py
@@ -1,3 +1,11 @@
-dictionary = {"Disdain":"the feeling that someone or something is unworthy of one's consideration or respect","Flourishing":"developing rapidly and successfully; thriving","Atrocity":"an extremely wicked or cruel act, typically one involving physical violence or injury","Fierce":"having or displaying an intense or ferocious aggressiveness"}
-word = input("Enter the word whose meaning you want : ")
-print(dictionary[word])
+s = set()
+# print(type(s))
+# l = [1, 2, 3, 4]
+# s_from_list = set(l)
+# print(s_from_list)
+# print(type(s_from_list))
+s.add(1)
+s.add(2)
+s.remove(2)
+s1 = {4, 6}
+print(s.isdisjoint(s1))
