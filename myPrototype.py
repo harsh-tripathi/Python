bc04320100ef5acd27ceaafd4091e83b8ca083de
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-02 15:24:09 +0530

    If Else & Elif Conditional

--- myPrototype.py
+++ myPrototype.py
@@ -1,11 +1,37 @@
-s = set()
-# print(type(s))
-# l = [1, 2, 3, 4]
-# s_from_list = set(l)
-# print(s_from_list)
-# print(type(s_from_list))
-s.add(1)
-s.add(2)
-s.remove(2)
-s1 = {4, 6}
-print(s.isdisjoint(s1))
+# var1 = 6
+# var2 = 56
+# var3 = int(input())
+# if var3>var2:
+#     print("Greater")
+# elif var3==var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
+
+# list1 = [5, 7, 3]
+# print(15 not in list1)
+# if 15 not in list1:
+#     print("No its not in the list")
+
+# Quiz
+print("What is your age?")
+age = int(input())
+if age < 0 :
+    print("Invalid Age")
+elif age > 60 and age < 90 :
+    print("You can drive but for your safety you shouldn't drive")
+elif age > 90:
+    print("Are you comedy me, how are you even alive")
+else:
+    if age<18:
+        print("You cannot drive")
+
+    elif age==18:
+        print("We will think about you")
+
+    else:
+        print("You can drive")
+
+
+
+
