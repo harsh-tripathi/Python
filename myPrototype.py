ae67f14a0f01e467b385b4ab21c3798374b3da0c
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-05 01:33:33 +0530

    Open(), Read() & Readline() For Reading File

--- myPrototype.py
+++ myPrototype.py
@@ -1,10 +1,31 @@
-print("Enter num1")
-num1 = input()
-print("Enter num2")
-num2 = input()
-try:
-    print("The sum of these two numbers is :", int(num1)+int(num2))
-except Exception as dog:
-    print(dog)
+# File IO Basics
+"""
+"r" - Open file for reading --> Default
+"w" - Open a file for writing
+"x" - Creates a file if not exists
+"a" - Add more content to a file
+"t" - text mode --> Default
+"b" - Binary mode 
+"+" - Read and Write
+"""
+
+f = open("harsh.txt", "rt") #Can leave 2nd argument Empty and will mean same
+print(f.readlines()) #This converts file into a list
+
+# print(f.readline())
+# print(f.readline())
+
+# cont = f.read()
+
+# for line in f:
+#     print(line, end="")
     
-print("This line is very important")
+# print(content)
+
+# content = f.read(34442)
+# print("1", content)
+
+# content = f.read(322222)
+# print("2", content)
+
+f.close()
