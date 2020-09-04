b7870f83084ff02e5718bcb7b7d192191f10def5
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-04 21:49:41 +0530

    Operators In Python

--- myPrototype.py
+++ myPrototype.py
@@ -1,23 +1,66 @@
-number = 30
-guess = 1
-while guess <= 9:
-    num = int(input("Guess a number : "))
-    if 9-guess == 0:
-        print("You ran out of guesses.")
-        print("Man you suck at this game.")
-        break
-    elif num < number:
-        print("Guess a bigger number.")
-        print("You have",9-guess,"guesses left.")
-        guess = guess+1
-        continue
-    elif num > number:
-        print("Guess a smaller number.")
-        print("You have",9-guess,"guesses left.")
-        guess = guess+1
-        continue
-    else:
-        print("Well Done, You guessed it correct!!")
-        print("Game Over!")
-        print("You answered correctly after",guess,"guesses.")
-        break
+# Operators in Python
+#Arithmetic Operator
+"""
+print("5 + 6 is",5+6)
+print("5 - 6 is",5-6)
+print("5 * 6 is",5*6)
+print("5 ** 6 is",5**6)
+print("5 / 6 is",5/6)
+print("5 // 6 is",5//6)
+print("5 % 6 is",5%6)
+
+"""
+#Assignment Operator
+"""
+x = 5
+x += 7
+x -= 7
+x /= 7
+x *= 7
+x %= 7
+
+"""
+#Comparison Operator
+"""
+i = 8
+print(i==5)
+print(i != 5)
+print(i >= 5)
+print(i <= 5)
+
+"""
+#Logical Operator
+"""
+a = True
+b = False
+print(a and b)
+print(a or b)
+
+"""
+#Identity Operator
+"""
+p = 2
+q = 7
+print(p is q)
+print(p is not q)
+
+"""
+#Membership Operator
+"""
+list = [3,4,5,7,11,88,44,666,23]
+print(23 in list)
+print(231 not in list)
+
+"""
+#Bitwise Operator
+"""
+In binary :
+    0 - 00
+    1 - 01
+    2 - 01
+    3 - 11
+print (0 & 1)
+print (0 | 1)
+print (2 | 1)
+
+"""
