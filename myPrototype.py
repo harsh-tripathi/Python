82a95e8064559c161c4b13adfc57d27cd4fb15c7
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-08 19:01:25 +0530

    Recursive Vs Iterative Approach

--- myPrototype.py
+++ myPrototype.py
@@ -1,25 +1,42 @@
-# l = 10 # Global
-#
-# def function1(n):
-#     # l = 5 #Local
-#     m = 8 #Local
-#     global l
-#     l = l + 45
-#     print(l, m)
-#     print(n, "I have printed")
-#
-# function1("This is me")
-# # print(m)
+# n! = n * n-1 * n-2 * n-3.......1
+# n! = n * (n-1)!
+def factorial_iterative(n):
+    """
+        :param n: Integer
+        :return: n * n-1 * n-2 * n-3.......1
+    """
+    fac = 1
+    for i in range(n):
+        fac = fac * (i+1)
+    return fac
 
-x = 89
-def harry():
-    x = 20
-    def rohan():
-        global x
-        x = 88
-    print("before calling rohan()", x)
-    rohan()
-    print("after calling rohan()", x)
+def factorial_recursive(n):
+    """
+        :param n: Integer
+        :return: n * n-1 * n-2 * n-3.......1
+    """
+    if n ==1:
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
+    # 5 * factorial_recursive(4)
+    # 5 * 4 * factorial_recursive(3)
+    # 5 * 4 * 3 * factorial_recursive(2)
+    # 5 * 4 * 3 * 2 * factorial_recursive(1)
+    # 5 * 4 * 3 * 2 * 1 = 120
 
-harry()
-print(x)
+# 0 1 1 2 3 5 8 13
+def fibonacci(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+ fibonacci(n-2)
+
+
+number = int(input("Enter then number"))
+# print("Factorial Using Iterative Method", factorial_iterative(number))
+# print("Factorial Using Recursive Method", factorial_recursive(number))
+print(fibonacci(number))
+  
