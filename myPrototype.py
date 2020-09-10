da20f71eddefebb02be439e01c5565805a1c1eee
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-10 22:32:56 +0530

    *args and **kwargs In Python

--- myPrototype.py
+++ myPrototype.py
@@ -1,39 +1,21 @@
-# Snake water gun
-# Create a snake water gun game in Python! Search Snake water gun game in google if you need help on rules and how to play the game!
-import random
-i = 0
-while i < 10 :
-    List = ["s","w","g"]
-    comp_sel = random. choice(List)
-    per_sel = input("Enter your choice : ")
-    if per_sel == "s" :
-        if comp_sel == "s" :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So it's a DRAW!")
-        elif comp_sel == "w" :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So you WON!")
-        else :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So you LOST!")
-    elif per_sel == "w" :
-        if comp_sel == "s" :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So you LOST!")
-        elif comp_sel == "w" :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So it's a DRAW!")
-        else :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So you WON!")
-    else :
-        if comp_sel == "s" :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So you WON!")
-        elif comp_sel == "w" :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So you LOST!")
-        else :
-            print("Computer Selected",comp_sel,"and you selected",per_sel)
-            print("So it's a DRAW!")
-    i = i+1
+# def function_name_print(a, b, c, d, e):
+#     print(a, b, c, d, e)
+
+def funargs(normal, *argsrohan, **kwargsbala):
+    print(normal)
+    for item in argsrohan:
+        print(item)
+    print("\nNow I would Like to introduce some of our heroes")
+    for key, value in kwargsbala.items():
+        print(f"{key} is a {value}")
+
+
+# function_name_print("Harry", "Rohan", "Skillf", "Hammad", "Shivam")
+
+har = ["Harry", "Rohan", "Skillf", "Hammad",
+       "Shivam", "The programmer"]
+normal = "I am a normal Argument and the students are:"
+kw = {"Rohan":"Monitor", "Harry":"Fitness Instructor",
+      "The Programmer": "Coordinator", "Shivam":"Cook"}
+funargs(normal, *har, **kw)
+  
