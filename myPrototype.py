f85d4a846c30dce25277453cb8f53ab06a41178c
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-11 21:14:44 +0530

    Time Module In Python

--- myPrototype.py
+++ myPrototype.py
@@ -1,21 +1,19 @@
-# def function_name_print(a, b, c, d, e):
-#     print(a, b, c, d, e)
+import time
+initial = time.time()
 
-def funargs(normal, *argsrohan, **kwargsbala):
-    print(normal)
-    for item in argsrohan:
-        print(item)
-    print("\nNow I would Like to introduce some of our heroes")
-    for key, value in kwargsbala.items():
-        print(f"{key} is a {value}")
+k = 0
+while(k<45):
+    print("This is harry bhai")
+    time.sleep(2)
+    k+=1
+print("While loop ran in", time.time() - initial, "Seconds")
+
+initial2 =time.time()
+for i in range(45):
+    print("This is harry bhai")
+print("For loop ran in", time.time() - initial2, "Seconds")
 
 
-# function_name_print("Harry", "Rohan", "Skillf", "Hammad", "Shivam")
-
-har = ["Harry", "Rohan", "Skillf", "Hammad",
-       "Shivam", "The programmer"]
-normal = "I am a normal Argument and the students are:"
-kw = {"Rohan":"Monitor", "Harry":"Fitness Instructor",
-      "The Programmer": "Coordinator", "Shivam":"Cook"}
-funargs(normal, *har, **kw)
+# localtime = time.asctime(time.localtime(time.time()))
+# print(localtime)
   
