591ae1ba732a4774e4a0daf5bee26468e49183d3
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-12 21:47:16 +0530

    Working of Import in Python and Different ways to import module

--- myPrototype.py
+++ myPrototype.py
@@ -1,11 +1,24 @@
-l1 = ["Bhindi", "Aloo", "chopsticks", "chowmein"]
+# Python File1 Code
 
-# i = 1
-# for item in l1:
-#     if i%2 is not 0:
-#         print(f"Jarvis please buy {item}")
-#     i += 1
+from sklearn.ensemble import RandomForestClassifier
+print(RandomForestClassifier())
 
-for index, item in enumerate(l1):
-    if index%2==0:
-        print(f"Jarvis please buy {item}")
+
+import file2
+print(file2.a)
+
+file2.printjoke("This is me")
+  
+# Python File2 Code
+
+a =7
+def printjoke(str):
+    print(f"this function is a joke {str}")
+  
+# Some Extra Knowledge
+
+import sys
+print( sys.path)
+import sklearn
+from flask import Flask
+import pandas as pd
