77f7c8b5cb6321b79c9c73e388b8dc04d938689a
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-13 21:13:06 +0530

    Start of Classes & Objects (OOPS) in Python

--- myPrototype.py
+++ myPrototype.py
@@ -1,36 +1,12 @@
-# def function1():
-#     print("Subscribe now")
-#
-# func2 = function1
-# del function1
-# func2()
+class Student:
+    pass
 
-# def funcret(num):
-#     if num==0:
-#         return print
-#     if num==1:
-#         return sum
-#
-# a = funcret(1)
-# print(a)
+harry = Student()
+larry = Student()
 
-# def executor(func):
-#     func("this")
-#
-#
-# executor(print)
-
-def dec1(func1):
-    def nowexec():
-        print("Executing now")
-        func1()
-        print("Executed")
-    return nowexec
-
-@dec1                                   # @ way to make a DECORATOR
-def who_is_harry():
-    print("Harry is a good boy")
-
-# who_is_harry = dec1(who_is_harry)      # Normal Way to make a DECORATOR
-
-who_is_harry()
+harry.name = "Harry"
+harry.std = 12
+harry.section = 1
+larry.std = 9
+larry.subjects = ["hindi", "physics"]
+print(harry.section, larry.subjects)
