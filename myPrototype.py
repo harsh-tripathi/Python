95dd0a438cab4f17abadf8ccd96a3a142268624a
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-16 13:47:15 +0530

    Single Inheritanc In Python

--- myPrototype.py
+++ myPrototype.py
@@ -21,8 +21,24 @@ class Employee:
     def printgood(string):
         print("This is good " + string)
 
+
+class Programmer(Employee):
+    no_of_holiday = 56
+    def __init__(self, aname, asalary, arole, languages):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+        self.languages = languages
+
+
+    def printprog(self):
+        return f"The Programmer's Name is {self.name}. Salary is {self.salary} and role is {self.role}.The languages are {self.languages}"
+
+
+
 harry = Employee("Harry", 255, "Instructor")
 rohan = Employee("Rohan", 455, "Student")
-karan = Employee.from_dash("Karan-480-Student")
 
-Employee.printgood("Rohan")
+shubham = Programmer("Shubham", 555, "Programmer", ["python"])
+karan = Programmer("Karan", 777, "Programmer", ["python", "Cpp"])
+print(karan.no_of_holiday)
