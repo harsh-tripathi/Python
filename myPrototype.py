3aa17c544fb599b74bea484a1a70e69825ff73f5
Harsh Tripathi <lko.atripathi@gmail.com>
2020-09-20 13:03:58 +0530

    Abstract Base Class & @abstractmethod In Python

--- myPrototype.py
+++ myPrototype.py
@@ -1,31 +1,21 @@
 
-class Employee:
-    no_of_leaves = 8
+# from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
-    def __init__(self, aname, asalary, arole):
-        self.name = aname
-        self.salary = asalary
-        self.role = arole
+class Shape(ABC):
+    @abstractmethod
+    def printarea(self):
+        return 0
 
-    def printdetails(self):
-        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+class Rectangle(Shape):
+    type = "Rectangle"
+    sides = 4
+    def __init__(self):
+        self.length = 6
+        self.breadth = 7
 
-    @classmethod
-    def change_leaves(cls, newleaves):
-        cls.no_of_leaves = newleaves
+    def printarea(self):
+        return self.length * self.breadth
 
-    def __add__(self, other):
-        return self.salary + other.salary
-
-    def __truediv__(self, other):
-        return self.salary / other.salary
-
-    def __repr__(self):
-        return f"Employee('{self.name}', {self.salary}, '{self.role}')"
-
-    def __str__(self):
-        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
-
-emp1 =Employee("Harry", 345, "Programmer")
-# emp2 =Employee("Rohan", 55, "Cleaner")
-print(str(emp1))
+rect1 = Rectangle()
+print(rect1.printarea())
